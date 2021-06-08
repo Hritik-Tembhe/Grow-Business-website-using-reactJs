f730dcf4a535b7dc8139d1e78d04aff13156867e
Hritik-Tembhe <84648355+Hritik-Tembhe@users.noreply.github.com>
2021-06-08 20:45:01 +0530

    Add files via upload

---
+++ src/Sdatapro.jsx
@@ -0,0 +1,36 @@
+import web from "../src/Images/WebD.png";
+import app from "../src/Images/AppD.jpg";
+import android from "../src/Images/AndDev.jpg"
+import digital from "../src/Images/DMark.jpg";
+import marketing from "../src/Images/Mark.jpg";
+import software from "../src/Images/SoftwareD.jpg";
+
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: 'Web Development',
+    },
+    {
+        imgsrc: app,
+        title: 'App Development',
+    },
+    {
+        imgsrc: android,
+        title: 'Android Development',
+    },
+    {
+        imgsrc: digital,
+        title: 'Digital Marketing',
+    },
+    {
+        imgsrc: marketing,
+        title: 'Marketing',
+    },
+    {
+        imgsrc: software,
+        title: 'Software Development',
+    },
+]
+
+export default Sdata;
